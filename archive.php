b25fe75917bf2bbe11af52c5ebcefe608454b74b
Ollie Palmer <github@olliepalmer.com>
2021-07-23 15:34:23 +0200

    minor pagination edits

--- archive.php
+++ archive.php
@@ -7,7 +7,7 @@
 						<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
 							<?php
-							the_archive_title( '<h1 class="page-title">', '</h1>' );
+							the_archive_title( '<h4 class="widgettitle">', '</h1>' );
 							the_archive_description( '<div class="taxonomy-description">', '</div>' );
 							?>
 
