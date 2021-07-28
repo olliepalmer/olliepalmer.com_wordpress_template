4ec7e71d44afb40cc35c230111f2604231db2988
Ollie Palmer <github@olliepalmer.com>
2021-07-28 16:58:46 +0200

    minor update

--- functions.php
+++ functions.php
@@ -251,11 +251,6 @@ function check_for_category_single_template( $t )
   foreach( (array) get_the_category() as $cat )
   {
     if ( file_exists(STYLESHEETPATH . "/single-{$cat->slug}.php") ) return STYLESHEETPATH . "/single-{$cat->slug}.php";
-    if($cat->parent)
-    {
-      $cat = get_the_category_by_ID( $cat->parent );
-      if ( file_exists(STYLESHEETPATH . "/single-{$cat->slug}.php") ) return STYLESHEETPATH . "/single-{$cat->slug}.php";
-    }
   }
   return $t;
 }
