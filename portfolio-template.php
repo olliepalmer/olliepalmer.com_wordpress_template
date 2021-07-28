b3b0e2af00f2dcd9bd7536d40bc3eb3fe0b91198
Ollie Palmer <github@olliepalmer.com>
2021-07-28 14:39:44 +0200

    update to hoem-image method

--- portfolio-template.php
+++ portfolio-template.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 
-/** 
+/**
 * Template name: Portfolio
 * Description: Page for displaying portfolio items.
 *
@@ -20,8 +20,8 @@
      ?>
      <a href="<?php the_permalink(); ?>">
      	<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID, 'thumbnail') ); ?>
-     	<div class="folio_image" style="background:url('<?php echo $url?>'); background-position:center; background-size:cover">
-     		<h1 style="text-align:center; line-height:400px;color:#e8e8e8;font-size:2em;letter-spacing:2px"><?php the_title(); ?></h1>
+     	<div class="folio_image" style="background:url('<?php echo $url?>');">
+     		<h1 class="home-title"><?php the_title(); ?></h1>
      	</div>
      </a>
  <?php endforeach; ?>
